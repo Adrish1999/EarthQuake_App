507c6bce0ef8a2b9147b55aac1c3ed0381bf9581
Adrish Bose <adrishbose198@gmail.com>
2020-08-11 10:12:48 +0530

    Update done

--- app/src/main/java/com/example/earthquake_app/MapsActivity.java
+++ app/src/main/java/com/example/earthquake_app/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.earthquake_app;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -23,6 +24,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
