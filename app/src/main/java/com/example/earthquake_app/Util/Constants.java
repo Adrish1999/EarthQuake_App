04747f904fe218dbf34184baf960ebb8938d9e18
Adrish Bose <adrishbose198@gmail.com>
2020-08-13 11:30:02 +0530

    Fetch

---
+++ app/src/main/java/com/example/earthquake_app/Util/Constants.java
@@ -0,0 +1,6 @@
+package com.example.earthquake_app.Util;
+
+public class Constants {
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    public static final int LIMIT = 30;
+}
